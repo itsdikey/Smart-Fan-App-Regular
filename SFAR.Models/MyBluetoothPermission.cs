0a22676c944c0ff78fd9700dc301554de9e75948
Garegin Tadevosyan <garo.tadevosyan@gmail.com>
2024-02-29 00:30:06 +0100

    new permission

---
+++ SFAR.Models/MyBluetoothPermission.cs
@@ -0,0 +1,16 @@
+﻿using static Microsoft.Maui.ApplicationModel.Permissions;
+
+namespace SFAR.Models
+{
+    public sealed class MyBluetoothPermission : BasePlatformPermission
+    {
+#if ANDROID
+        public override (string androidPermission, bool isRuntime)[] RequiredPermissions =>
+            new List<(string permission, bool isRuntime)>
+            {
+                ("android.permission.BLUETOOTH_SCAN", true),
+                ("android.permission.BLUETOOTH_CONNECT", true)
+            }.ToArray();
+#endif
+    }
+}
--- SFAR.Services/Implementations/RequiredPermissionService.cs
+++ SFAR.Services/Implementations/RequiredPermissionService.cs
@@ -1,50 +1,80 @@
-﻿using static Microsoft.Maui.ApplicationModel.Permissions;
+﻿using SFAR.Models;
+using static Microsoft.Maui.ApplicationModel.Permissions;
 
 namespace SFAR.Services.Implementations
 {
     internal class RequiredPermissionService : IRequiredPermissionService
     {
-        public async Task<bool> HasAllPermissions()
+        public Task<bool> HasAllPermissions()
         {
-            PermissionStatus status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
 
-            if(status != PermissionStatus.Granted)
+            TaskCompletionSource<bool> taskResult = new TaskCompletionSource<bool>();
+
+            MainThread.BeginInvokeOnMainThread(async () =>
             {
-                var result = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+                PermissionStatus status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
 
-                if(result != PermissionStatus.Granted)
+                if (status != PermissionStatus.Granted)
                 {
-                    return false;
+                    var result = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+
+                    if (result != PermissionStatus.Granted)
+                    {
+                        taskResult.SetResult(false);
+                        return;
+                    }
                 }
-            }
 
-            status = await Permissions.CheckStatusAsync<Permissions.Bluetooth>();
+                status = await Permissions.CheckStatusAsync<Permissions.Bluetooth>();
 
-            if (status != PermissionStatus.Granted)
-            {
-                var result = await Permissions.RequestAsync<Permissions.Bluetooth> ();
-
-                if (result != PermissionStatus.Granted)
+                if (status != PermissionStatus.Granted)
                 {
-                    return false;
+                    var result = await Permissions.RequestAsync<Permissions.Bluetooth>();
+
+                    if (result != PermissionStatus.Granted)
+                    {
+                        taskResult.SetResult(false);
+                        return;
+                    }
                 }
-            }
 
 
-            status = await Permissions.CheckStatusAsync<Permissions.NearbyWifiDevices>();
+                status = await Permissions.CheckStatusAsync<Permissions.NearbyWifiDevices>();
 
-            if (status != PermissionStatus.Granted)
-            {
-                var result = await Permissions.RequestAsync<Permissions.NearbyWifiDevices>();
-
-                if (result != PermissionStatus.Granted)
+                if (status != PermissionStatus.Granted)
                 {
-                    return false;
+                    var result = await Permissions.RequestAsync<Permissions.NearbyWifiDevices>();
+
+                    if (result != PermissionStatus.Granted)
+                    {
+                        taskResult.SetResult(false);
+                        return;
+                    }
                 }
-            }
+
+                if (DeviceInfo.Version.Major >= 12)
+                {
+                    status = await Permissions.CheckStatusAsync<MyBluetoothPermission>();
+
+                    if (status != PermissionStatus.Granted)
+                    {
+                        var result = await Permissions.RequestAsync<MyBluetoothPermission>();
+
+                        if (result != PermissionStatus.Granted)
+                        {
+                            taskResult.SetResult(false);
+                            return;
+                        }
+                    }
+                }
+
+              
 
 
-            return true;
+                taskResult.SetResult(true);
+            });
+          
+            return taskResult.Task; 
         }
     }
 }
